199580a9b1d390586e11bd8a0c11155f49f55cba
Josias26 <josiasmiguelps@gmail.com>
2026-04-02 11:40:19 -0400

    Logica de negocio

---
+++ Tarea 3-4_P3/Models/Dbcrudcore01Context.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tarea_3_4.Models;
+
+public partial class Dbcrudcore01Context : DbContext
+{
+    public Dbcrudcore01Context()
+    {
+    }
+
+    public Dbcrudcore01Context(DbContextOptions<Dbcrudcore01Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.ID).HasName("PK__Empleado__3213E81F6A7C5EAF");
+
+            entity.ToTable("Empleado");
+
+            entity.Property(e => e.ID).HasColumnName("iD");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("direccion");
+            entity.Property(e => e.Edad).HasColumnName("edad");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Numero)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("numero");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Tarea 3-4_P3/Models/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarea_3_4.Models;
+
+public partial class Empleado
+{
+    public int ID { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public int? Edad { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public string? Numero { get; set; }
+
+    public string? Email { get; set; }
+}
